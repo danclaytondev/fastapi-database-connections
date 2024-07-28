9eded391cf0ce98cf44a26d4fff91caacf667cd0
Dan Clayton <daniel@cl8on.co.uk>
2024-07-28 15:56:07 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Setting Up Database Connections using FastAPI
+
+Code setup here based on blog post: https://blog.danielclayton.co.uk/posts/database-connections-with-fastapi
+
+## Running 
+
+1. Install [rye](https://rye.astral.sh/).
+
+2. `$ rye run fastapi dev src/fastapi_database_connections/main.py`
---
+++ src/fastapi_database_connections/main.py
@@ -0,0 +1,29 @@
+from contextlib import asynccontextmanager
+from fastapi import Depends, FastAPI
+import psycopg_pool
+import psycopg
+
+conn_string = "postgres://postgres@localhost"
+
+pool = psycopg_pool.AsyncConnectionPool(conn_string, open=False)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await pool.open()
+    yield
+    await pool.close()
+
+app = FastAPI(lifespan=lifespan)
+
+async def get_conn():
+    async with await psycopg.AsyncConnection.connect(conn_string) as conn:
+	    yield conn
+
+@app.get("/visit/")
+async def add_visit(conn = Depends(get_conn)):
+
+    async with conn.cursor() as cursor:
+        # Run our queries
+        await cursor.execute("insert into visits(timestamp) values (now())")
+        
+    return {"message": "Visit logged"}
